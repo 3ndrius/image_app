e72a1b964eccbc32c76ad2324470a1b42a66b5fb
3ndrius <andrzejo1991@gmail.com>
2019-05-22 18:03:45 +0200

    Get data from unsplash

--- App.js
+++ App.js
@@ -1,11 +1,41 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ActivityIndicator, FlatList } from 'react-native';
+
+import { secretKey, publicKey} from './config';
+
 
 export default class App extends React.Component {
+
+  state ={
+    isLoading:true,
+    images: []
+  }
+  loadImages = () => {
+      fetch(`https://api.unsplash.com/photos/?client_id=${publicKey}`).then(
+       data => {
+          console.log(data);
+          this.setState({
+            image: data,
+            isLoading: false
+          })
+       }).catch(err => {
+         console.log(err);
+       }).finally(() => {
+         console.log("Completed request");
+       })
+  }
+  componentDidMount = () =>{
+    this.loadImages();
+  }
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Hello</Text>
+      this.state.isLoading ? 
+      <View style={{flex:1, backgroundColor:'black', alignItems:'center', justifyContent:'center' }}>
+        <ActivityIndicator size='large' color="gray" />
+      </View> 
+      :
+      <View style={{flex:1, backgroundColor:'white', alignItems:'center', justifyContent:'center' }}>
+        <FlatList />
       </View>
     );
   }
@@ -13,9 +43,6 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  
   },
 });
