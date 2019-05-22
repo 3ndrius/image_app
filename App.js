94887af2d73021789f4a3599f008213fc4480aa2
3ndrius <andrzejo1991@gmail.com>
2019-05-22 19:14:43 +0200

    Adding axios , fetch image from unspash and display as a list

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View, ActivityIndicator, FlatList } from 'react-native';
+import { StyleSheet, Text, View, ActivityIndicator, FlatList, Dimensions, Image } from 'react-native';
+import axios from 'axios';
+import {  publicKey} from './config';
 
-import { secretKey, publicKey} from './config';
-
+const { height, width} = Dimensions.get('window');
 
 export default class App extends React.Component {
 
@@ -11,32 +12,43 @@ export default class App extends React.Component {
     images: []
   }
   loadImages = () => {
-      fetch(`https://api.unsplash.com/photos/?client_id=${publicKey}`).then(
-       data => {
-          console.log(data);
-          this.setState({
-            image: data,
-            isLoading: false
-          })
-       }).catch(err => {
-         console.log(err);
-       }).finally(() => {
-         console.log("Completed request");
+       axios
+       .get(`https://api.unsplash.com/photos/random?count=30&client_id=${publicKey}`)
+       .then((response) => {
+           console.log(response.data);
+           this.setState({ images: response.data, isLoading: false });
+         }
+       )
+       .catch((error) => {
+         console.log(error);
        })
+       .finally( () => {
+         console.log('request completed');
+       });
+  }
+  renderItem(image) {
+    return <View style={{ height, width }}> <Image style={{flex:1,height:null, width:null}} source={{uri: image.urls.regular}}  /></View>
   }
   componentDidMount = () =>{
     this.loadImages();
   }
   render() {
+    console.log("Data", this.state.images);
     return (
       this.state.isLoading ? 
       <View style={{flex:1, backgroundColor:'black', alignItems:'center', justifyContent:'center' }}>
         <ActivityIndicator size='large' color="gray" />
       </View> 
       :
-      <View style={{flex:1, backgroundColor:'white', alignItems:'center', justifyContent:'center' }}>
-        <FlatList />
-      </View>
+      <View style={{ flex: 1, backgroundColor: 'yellow' }}>
+      <FlatList
+        horizontal
+        pagingEnabled
+        data={this.state.images}
+        renderItem={({ item }) => this.renderItem(item)}
+        keyExtractor={item => item.id}
+      />
+    </View>
     );
   }
 }
