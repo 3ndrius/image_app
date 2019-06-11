f7346a33aa8d5261d4dfb96f509160c519e7e367
3ndrius <andrzejo1991@gmail.com>
2019-06-11 10:06:57 +0200

    Adding icon

--- App.js
+++ App.js
@@ -1,82 +1,185 @@
 import React from 'react';
-import { StyleSheet, View, ActivityIndicator, FlatList, Dimensions, Image, TouchableWithoutFeedback } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  ActivityIndicator,
+  FlatList,
+  Dimensions,
+  Image,
+  Animated,
+  TouchableWithoutFeedback,
+  TouchableOpacity
+} from 'react-native';
+
 import axios from 'axios';
-import {  publicKey } from './config';
-
-const { height, width} = Dimensions.get('window');
-
+import { Ionicons } from '@expo/vector-icons';
+const { height, width } = Dimensions.get('window');
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoading: true,
+      images: [],
+      scale: new Animated.Value(1),
+      isImageFocused: false
+    };
 
-  state ={
-    isLoading:true,
-    images: [],
-    scale: new Animated.Value(1),
-    isImageFocused: false
+    this.scale = {
+      transform: [{ scale: this.state.scale }]
+    };
+
+    this.actionBarY = this.state.scale.interpolate({
+      inputRange: [0.9, 1],
+      outputRange: [0, -80]
+    });
   }
-  loadImages = () => {
-       axios
-       .get(`https://api.unsplash.com/photos/random?count=30&client_id=${publicKey}`)
-       .then((response) => {
-           console.log(response.data);
-           this.setState({ images: response.data, isLoading: false });
-         }
-       )
-       .catch((error) => {
-         console.log(error);
-       })
-       .finally( () => {
-         console.log("Work");
-       });
+
+  loadWallpapers = () => {
+    axios
+      .get(
+        'https://api.unsplash.com/photos/random?count=30&client_id=896979fdb70f80865638d7a4648bf9ce309675335318933eab2bf990af42e295'
+      )
+      .then(
+        function(response) {
+          console.log(response.data);
+          this.setState({ images: response.data, isLoading: false });
+        }.bind(this)
+      )
+      .catch(function(error) {
+        console.log(error);
+      })
+      .finally(function() {
+        console.log('request completed');
+      });
+  };
+
+  componentDidMount() {
+    this.loadWallpapers();
   }
-  this.state.scale = {
-    transform: [{scale: this.state.scale}]
-  }
-  showControls = (item) => {
-    this.setState((state) => {
-      isImageFocused: !state.isImageFocused
-    }),()=> {
-      if(this.state.isImageFocused) {
-        Animated.spring(this.state.scale, {
-          toValue: 0.8
-        }).start()
+
+  showControls = item => {
+    this.setState(
+      state => ({
+        isImageFocused: !state.isImageFocused
+      }),
+      () => {
+        if (this.state.isImageFocused) {
+          Animated.spring(this.state.scale, {
+            toValue: 0.9
+          }).start();
+        } else {
+          Animated.spring(this.state.scale, {
+            toValue: 1
+          }).start();
+        }
       }
-      else{
-        Animated.spring(this.state.scale, {
-          toValue:1
-        }).start()
-      }
-    }
-  }
-  renderItem = ({item}) => {
-    return <View style={{ height, width }}><Image style={{flex:1,height:null, width:null}} source={{uri:item.urls.regular}}/></View>
-   
-  }
-  componentDidMount = () =>{
-    this.loadImages();
-  }
-  render() {
-  
+    );
+  };
+
+  renderItem = ({ item }) => {
     return (
-      this.state.isLoading ? 
-      <View style={{flex:1, backgroundColor:'black', alignItems:'center', justifyContent:'center' }}>
-        <ActivityIndicator size='large' color="gray" />
-      </View>:
-      <TouchableWithoutFeedback onPress={() => {showControls(item)}}>
-      <Animated.View style={[{flex: 1, backgroundColor: 'white' }, this.scale]}>
-      <FlatList
-        horizontal
-        pagingEnabled
-        data={this.state.images}
-        renderItem={this.renderItem}
-        keyExtractor={item => item.id}/>
-        </Animated.View>
+      <View style={{ flex: 1 }}>
+        <View
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            backgroundColor: 'black',
+            alignItems: 'center',
+            justifyContent: 'center'
+          }}
+        >
+          <ActivityIndicator size="large" color="grey" />
+        </View>
+        <TouchableWithoutFeedback onPress={() => this.showControls(item)}>
+          <Animated.View style={[{ height, width }, this.scale]}>
+            <Image
+              style={{ flex: 1, height: null, width: null }}
+              source={{ uri: item.urls.regular }}
+            />
+          </Animated.View>
         </TouchableWithoutFeedback>
+        <Animated.View
+          style={{
+            position: 'absolute',
+            left: 0,
+            right: 0,
+            bottom: this.actionBarY,
+            height: 80,
+            backgroundColor: 'black',
+            flexDirection: 'row',
+            justifyContent: 'space-around'
+          }}
+        >
+          <View
+            style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+          >
+            <TouchableOpacity
+              activeOpacity={0.5}
+              onPress={() => alert('load images')}
+            >
+              <Ionicons name="ios-refresh" color="white" size={40} />
+            </TouchableOpacity>
+          </View>
+          <View
+            style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+          >
+            <TouchableOpacity
+              activeOpacity={0.5}
+              onPress={() => alert('load images')}
+            >
+              <Ionicons name="ios-share" color="white" size={40} />
+            </TouchableOpacity>
+          </View>
+          <View
+            style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+          >
+            <TouchableOpacity
+              activeOpacity={0.5}
+              onPress={() => alert('load images')}
+            >
+              <Ionicons name="ios-save" color="white" size={40} />
+            </TouchableOpacity>
+          </View>
+        </Animated.View>
+      </View>
+    );
+  };
+  render() {
+    return this.state.isLoading ? (
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: 'black',
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}
+      >
+        <ActivityIndicator size="large" color="grey" />
+      </View>
+    ) : (
+      <View style={{ flex: 1, backgroundColor: 'black' }}>
+        <FlatList
+          scrollEnabled={!this.state.isImageFocused}
+          horizontal
+          pagingEnabled
+          data={this.state.images}
+          renderItem={this.renderItem}
+          keyExtractor={item => item.id}
+        />
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-  
-  },
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
 });
